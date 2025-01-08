a069782f07749bb9bfc0f2f1142840c29c8c6291
Yogita Shete <yogitashete24@gmail.com>
2025-01-08 21:47:30 +0530

    Add RegisterForm component and integrate into RegisterPage: Implemented a new registration form with email, password, and confirm password fields. Enhanced UI with Tailwind CSS for better styling and user experience. Updated RegisterPage to include the new RegisterForm component for user registration flow.

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,12 @@
+import RegisterForm from '@/components/Register/RegisterForm'
 import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>
-      RegisterPage
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+      <RegisterForm/>
+    </div>
     </div>
   )
 }
