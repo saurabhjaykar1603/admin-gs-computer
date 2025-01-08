6cff3063f9e970105bc55594f91e936f74d9d157
Yogita Shete <yogitashete24@gmail.com>
2025-01-08 16:37:37 +0530

    Add configuration for Tailwind CSS and Radix UI components. Introduced components.json for component aliases, updated package dependencies including Tailwind CSS and Radix UI, and enhanced Tailwind configuration with custom theme settings. Added utility functions for class management and a Button component for UI consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
      <p className="text-green-600">Welcome to the GS Admin</p>
+     <Button>Click me</Button>
     </>
   )
 }
