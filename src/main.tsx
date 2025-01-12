83be9e84cd731d04a1a6892a34febecfbe2f8ba9
Saurabh Jaykar <jaykarsaurabh97@gmail.com>
2025-01-12 16:07:37 +0530

    Refactor main entry point to include React Query and Toaster: Wrapped the application in QueryClientProvider for data fetching and added Toaster component for notifications. This enhances state management and user feedback in the app.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router.tsx'
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/toaster.tsx";
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+    <Toaster />
+  </React.StrictMode>
+);
