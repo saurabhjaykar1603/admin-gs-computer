c5d24b3bd5a841694625a3a4b564ad3b5d624a3a
Saurabh Jaykar <jaykarsaurabh97@gmail.com>
2025-01-12 16:58:51 +0530

    Refactor product management pages: Updated AddProduct and ProductsPage components to enhance layout and user experience. Improved header styling, added action buttons for product management, and ensured consistent design across both pages.

--- src/pages/products/AddProduct.tsx
+++ src/pages/products/AddProduct.tsx
@@ -1,17 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { List } from "lucide-react";
 
 function AddProduct() {
   return (
     <div>
-      <div className="flex flex-col gap-2">
-        <h1 className="text-3xl font-bold tracking-tight md:text-4xl lg:text-4xl">
-          Add Product
-        </h1>
-        <p className="text-sm text-muted-foreground md:text-base">
-          Create and manage your product inventory
-        </p>
+      <div className="flex flex-row justify-between items-center gap-2">
+        <div>
+          <h1 className="text-3xl flex gap-3 font-bold tracking-tight items-center md:text-4xl lg:text-4xl mb-2 text-red-600">
+            Add Products
+          </h1>
+          <p className="text-sm text-muted-foreground md:text-base text-red-500">
+            Create and manage your product inventory
+          </p>
+        </div>
+        <div>
+          <Button variant={"destructive"}>
+            {" "}
+            <List className="text-3xl" />{" "}
+            <span className="hidden md:block">Product List</span>
+          </Button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default AddProduct
+export default AddProduct;
--- src/pages/products/ProductsPage.tsx
+++ src/pages/products/ProductsPage.tsx
@@ -1,13 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 function ProductPage() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+    <>
+      <div className="flex flex-row justify-between items-center gap-2">
+        <div>
+          <h1 className="text-3xl font-bold tracking-tight md:text-4xl lg:text-4xl mb-2 text-red-600">
+            ALL Products
+          </h1>
+          <p className="text-sm text-muted-foreground md:text-base text-red-500">
+            Create and manage your product inventory
+          </p>
+        </div>
+        <div>
+          <Button variant={"destructive"}>
+            {" "}
+            <PlusCircle className="text-3xl" /> Add Product
+          </Button>
+        </div>
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-    </div>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+        </div>
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
+    </>
   );
 }
 
