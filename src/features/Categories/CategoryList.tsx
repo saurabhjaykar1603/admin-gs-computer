fac725433a889ae0a91b9d8d458986cc21c22464
Saurabh Jaykar <jaykarsaurabh97@gmail.com>
2025-01-14 20:00:22 +0530

    Refactor category and product management: Enhanced category management with improved loading and error handling in `CategoryList`, and optimized product management by refining hooks for fetching, creating, updating, and deleting products. This update improves user experience and type safety across the application.

---
+++ src/features/Categories/CategoryList.tsx
@@ -0,0 +1,47 @@
+import { useToast } from "@/hooks/use-toast";
+import { Category } from "@/types/Category";
+import { useEffect } from "react";
+import CategoryCard from "./CategoryCard";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface CategoryListProps {
+  categories: Category[];
+  isLoading: boolean;
+  error: Error | null;
+}
+
+function CategoryList({ categories, isLoading, error }: CategoryListProps) {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
+  }, [error, toast]);
+
+  if (isLoading) return (
+    <div className="space-y-4">
+      {[...Array(13)].map((_, i) => (
+        <div key={i} className="flex items-center space-x-4">
+          <div className="h-12 w-full">
+            <Skeleton className="h-full w-full rounded" />
+          </div>
+        
+        </div>
+      ))}
+    </div>
+  );
+  return (
+    <div className="space-y-4">
+      {categories.map((category) => (
+        <CategoryCard key={category._id} name={category.name} id={category._id} />
+      ))}
+    </div>
+  );
+}
+
+export default CategoryList;
