a68b611da8a35225b9e74619367ab12526839732
Saurabh Jaykar <jaykarsaurabh97@gmail.com>
2025-01-18 14:09:29 +0530

    Refactor CategoryCard component: Removed Pencil icon and replaced it with the new EditCategoryButton component for editing categories. This change streamlines the UI by consolidating edit functionality and enhances the overall user experience in category management.

--- src/features/Categories/CategoryCard.tsx
+++ src/features/Categories/CategoryCard.tsx
@@ -1,15 +1,13 @@
-import { Pencil, Trash2 } from "lucide-react";
+import { Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import EditCategoryButton from "./EditCategoryButton";
 
 function CategoryCard({ name, id }: { name: string; id: string }) {
-  console.log(id);
   return (
     <div className="flex items-center justify-between p-4 bg-white rounded-lg shadow">
       <div className="font-medium">{name}</div>
       <div className="flex items-center gap-2">
-        <Button variant="ghost" size="icon">
-          <Pencil className="h-4 w-4" />
-        </Button>
+        <EditCategoryButton defaultData={name} id={id} />
         <Button variant="ghost" size="icon">
           <Trash2 className="h-4 w-4 text-red-500" />
         </Button>
